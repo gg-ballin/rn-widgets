da8a8d978ef5bee7e9581b593d81e0b91cadaeea
Germán Gómez <gomezger.a9@gmail.com>
2025-10-20 10:40:00 -0300

    deleted unwanted code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         </Stack>
         <StatusBar style="auto" />
---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+      <Text style={{ fontSize: 20, color: 'white' }}>Home</Text>
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
